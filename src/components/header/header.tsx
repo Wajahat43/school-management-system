989ceec19ae8e6dbc2e3593c723ef60cb1d54442
Wajahat Islam Gul <aliwajahat44@yahoo.com>
2024-07-28 20:06:30 +0500

    Add header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  const navLinks = [
+    { title: "Teachers", path: "/teachers" },
+    { title: "Students", path: "/students" },
+    { title: "Subjects", path: "/subjects" },
+  ];
+  return (
+    <header className="flex flex-col items-center justify-between gap-2 px-24 py-4 min-h-20 md:flex-row">
+      <Link className="hover:scale-105" href="/">
+        Home
+      </Link>
+      <nav className="flex flex-col justify-between gap-2 md:gap-10 md:flex-row ">
+        {navLinks.map(({ title, path }) => (
+          <Link key={title} className="hover:scale-105" href={path}>
+            {title}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
